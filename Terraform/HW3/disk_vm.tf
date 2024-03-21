af471df751897bdeacd0018c2e024f41fbf2dafa
Adel-pro <116494871+Adel-pro@users.noreply.github.com>
2024-03-21 18:45:57 +0300

    21.03v1

--- Terraform/HW3/disk_vm.tf
+++ Terraform/HW3/disk_vm.tf
@@ -22,7 +22,7 @@ resource "yandex_compute_instance" "storage" {
     dynamic "secondary_disk" {
         for_each = "${yandex_compute_disk.default.*.id}"
         content  {
-            disk_id = yandex_compute_disk.default["${secondary_disk.key}"].id
+            disk_id = secondary_disk.value
         }        
     }
     scheduling_policy {
